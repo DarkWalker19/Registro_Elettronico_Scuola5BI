c8cb68e8fe046e6e160d1b41c19d88ee6a6d7fa2
DanieleAgati <102727095+DanieleAgati@users.noreply.github.com>
2022-05-05 19:33:35 +0200

    Fix

---
+++ utils/footer.php
@@ -0,0 +1,78 @@
+<?php
+    require_once("utils.php");
+    get_css();
+?>
+
+<html>
+    <head></head>
+    <body>
+        <footer class="text-center text-lg-start bg-light text-muted">
+            <!-- Section: Social media -->
+            <section
+            class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+            >
+            </section>
+            <!-- Section: Social media -->
+        
+            <!-- Section: Links  -->
+            <section>
+            <div class="container text-center text-md-start mt-5">
+                <!-- Grid row -->
+                <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Il sito è stato realizzato da
+                    </h6>
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Gruppo 3
+                    </h6>
+                    <p>
+                        Agati Daniele<br>
+                        Brecani Arber<br>
+                        Mersoni Marco<br>
+                        Patratanu Alexandru<br>
+                        Ricchini Christian<br>
+                        Serci Riccardo (C)<br>
+                    </p>
+                </div>
+                <!-- Grid column -->
+        
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Link utili
+                    </h6>
+                    <p onclick="location.href = 'https://www.itiscardanopv.edu.it/';">
+                        Itis G. Cardano
+                    </p>
+                </div>
+                <!-- Grid column -->
+        
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                    Contatto
+                    </h6>
+                    <p>Via Verdi, 19<br>
+                    27100 - Pavia, Italia</p>
+                    <p>rserci.s@itiscardano.edu.it</p><br>
+
+                </div>
+                <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+            </section>
+            <!-- Section: Links  -->
+        
+            <!-- Copyright -->
+            <div class="text-center p-4" style="background-color: rgba(70, 112, 250, 0.993);">
+            </div>
+            <!-- Copyright -->
+        </footer>
+    </body>
+</html>
--- utils/header.php
+++ utils/header.php
@@ -1,31 +1,48 @@
+<?php
+    require_once "utils.php";
+    get_css();
+?>
+
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <nav class="navbar navbar-light" style="background-color: #FFD700;">
-    </head>
+    <head></head>
 
     <body>
-        <div><h1>REGISTRO ELETTRONICO</h1></div>
-        <img src="IMG/logo.jpg" alt="LogoRegistro" height="60" width="60" border="2" align="right" hspace="10"></img>
+        <nav class="navbar navbar-light" style="background-color: #3366FF;">
+        <div class="container-fluid">
+        <a class="navbar-brand" style="color: white;" href="index.php">Registro Elettronico</a>
+        <a href="https://www.itiscardanopv.edu.it/">
+            <img src="img/logo.jpg" alt="LogoRegistro" height="60" width="60" border="2" align="right" hspace="10"></img>
+        </a>
     </nav>
 
     <?php
+<<<<<<< Updated upstream
         if(!isset($_SESSION['user'])){
             echo '<nav class="navbar navbar-expand-lg" style="background-color: #f0e68c">';
             echo "<button onclick=\"location.href='login.php'\" type=\"button\" align=\"right\" hspace=\"10\">Login</button></nav>";
         }else{
             echo '<nav class="navbar navbar-expand-lg" style="background-color: #f0e68c">';
+=======
+        //if(!isset($_SESSION['user']) || $_SESSION['user'] == null){
+            //echo '<nav class="navbar navbar-expand-lg" style="background-color: #6699FF">';
+            //echo "<button onclick=\"location.href='login.php'\" type=\"button\" align=\"right\" hspace=\"10\">Login</button></nav>";
+        //}else{
+            echo '<nav class="navbar navbar-expand-lg" style="background-color: #6699FF">';
+            echo '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">';
+            echo '<span class="navbar-toggler-icon"></span></button>';
+>>>>>>> Stashed changes
             echo    '<div class="collapse navbar-collapse" id="navbarNav">';
             echo        '<ul class="navbar-nav">';
             echo            '<li class="nav-item">';
-            echo                '<a class="nav-link" href="#">Assenze</a></li>';
+            echo                '<a class="nav-link" href = \'agenda.php?section=a\'>Assenze</a></li>';
             echo            '<li class="nav-item">';
-            echo                '<a class="nav-link" href="#">Ritardi</a></li>';            
+            echo                '<a class="nav-link" href = \'agenda.php?section=r\'>Ritardi</a></li>';            
             echo            '<li class="nav-item">';
-            echo                '<a class="nav-link" href="#">Uscita anticipate</a></li></ul></div>';
-            echo    "<button onclick=\"location.href='logout.php'\" type=\"button\" align=\"right\" hspace=\"10\">Logout</button></nav>";
-        }
+            echo                '<a class="nav-link" href = \'agenda.php?section=u\'>Uscite anticipate</a></li></ul></div>';
+            echo    "<button class=\"btn btn-primary\" style=\"background-color: #98CCFF;\" onclick=\"location.href='logout.php'\" type=\"button\" align=\"right\" hspace=\"10\">Logout</button></nav>";
+        //}
     ?>
     
 </html>
