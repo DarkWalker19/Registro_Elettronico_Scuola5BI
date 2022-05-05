721ea12b07bf26314450070285f9bfaf4ed6c462
DarkWalker19 <rickyserci@gmail.com>
2022-05-05 19:29:13 +0200

    additions

--- utils/utils.php
+++ utils/utils.php
@@ -34,7 +34,7 @@
     }
 
     function get_css(){
-        echo "<link rel='icon' href='.\img\logo.jpg'>";
+        echo "<link rel='icon' href='.\img\logo.png'>";
         echo "<link href='./css/main.css' rel='stylesheet'></link>";
         echo "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'></link>";
         echo '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
