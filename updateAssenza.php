7b25222482bbef07e100939e74e0811a0a548647
MarcoMersoni <102726687+MarcoMersoni@users.noreply.github.com>
2022-05-10 11:45:44 +0200

    Add files via upload
    Upload 10/05/2022

--- updateAssenza.php
+++ updateAssenza.php
@@ -3,12 +3,25 @@
 
     is_user_logged();
 	
-	$date = $_POST['date'];
-	$hour = $_POST['hour'];
+	
+	if(!isset($_POST['id']) || !isset($_POST['date']) || !isset($_POST['motivation'])) error("invalid");
+	
 	$id = $_POST['id'];
+	$date = $_POST['date'];
 	$motivation = $_POST['motivation'];
 	
-	UPDATE Evento SET Motivazione = $motivation, Stato = ‘’ WHERE (Id = $id);
+	if(!check_role("admin")){
+		$state = "In attesa";
+	}else{
+		$state = "Giustificato";
+	}
+	
+	$db = get_PDO_connection();
+	
+	$query = "UPDATE Evento SET Motivazione = $motivation, Stato = $state WHERE (Id = $id)";
+	
+	$result = $db->prepare($query);
+    $result->execute([$id, $date, $motivation]);
 
     
 	?>
