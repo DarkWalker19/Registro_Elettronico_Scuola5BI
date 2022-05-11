a2e61007c4aa5592906acb36a01482faec3f3218
Christian <102727287+Chry02@users.noreply.github.com>
2022-05-11 17:24:46 +0200

    Update index.php
    index

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
-<!DOCTYPE html>
-
-<html>
+<?php
+    require_once "utils/utils.php";
+?>
+<!doctype html>
+<html lang="it">
     <head>
-        <title>INDEX</title>
+        <?php
+            print_header();
+            get_css();
+        ?>
+  
+        <title>Pagina Iniziale</title>
     </head>
-
     <body>
+        <?php
+            if((!isset($_SESSION['Name'])) || ($_SESSION['Name'] == null)){
+                echo "<br>";echo"<br>";echo"<br>";echo"<br>";echo"<br>";echo"<br>";echo"<br>";
+                echo "<h4>Non sei loggato, accedi tramite il bottone LOGIN per poter continuare!</h4>";
+                echo "<br>";echo"<br>";echo"<br>";echo"<br>";echo"<br>";echo"<br>";echo"<br>";
+            }else{
+                echo "<p>Benvenuto". " " .$_SESSION['Name'];
+            }
+            print_footer();
+        ?>
     </body>
 </html>
