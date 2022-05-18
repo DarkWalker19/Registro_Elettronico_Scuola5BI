06dbba06b49221f4642fce4541269571612f4cbb
Christian <102727287+Chry02@users.noreply.github.com>
2022-05-18 17:26:46 +0200

    Update addRitardo.php
    addRitardo

--- addRitardo.php
+++ addRitardo.php
@@ -1,18 +1,29 @@
 <?php
-	require "utils/utils.php";
+	require_once "utils/utils.php";
 
     is_user_logged();
-	
-	if(!isset($_POST['date']) || !isset($_POST['hour'])) error("invalid");
 
-	$date = $_POST['date'];
-	$hour = $_POST['hour'];
+	if(check_role("parent") || $_SESSION['old']){
 	
-	$query = "INSERT INTO Evento (Data, Ora_entrata) VALUES ('$date', '$hour');"
-	
-	$db = get_PDO_connection();
-	
-	$result = $db->prepare($query);
-    $result->execute([$date,$hour]);
+		if(!isset($_POST['data']) || !isset($_POST['hour']) || !isset($_POST['motivation'])) error("invalid");
 
+		$db = get_PDO_connection();
+		$date = $_POST['date'];
+		$hour = $_POST['hour'];
+		$motivation = $_POST['motivation'];
+		$numb = $_POST['matricola'];
+		
+		$query = "INSERT INTO Evento (Stato,Data,Ora_entrata,Motivazione,Tipo,U_Matricola)";
+		$query .= "VALUES ('4',?,?,?,'2',?);";
+		
+		try{
+			$result = $db->prepare($query);
+			$result->execute([$date,$hour,$motivation,$numb]);	
+		}catch(PDOException $e){
+			error("PDO_QUERY_Exception");
+		}
+
+	}else{
+		error("insufficient permission");
+	}
 ?>
