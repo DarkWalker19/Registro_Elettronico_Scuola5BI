a7de276c1664afea1394883a036202efb7adc821
DanieleAgati <102727095+DanieleAgati@users.noreply.github.com>
2022-05-24 21:22:12 +0200

    Updates

--- updateUscita.php
+++ updateUscita.php
@@ -19,41 +19,34 @@
 			if(!isset($_POST['date']) || !isset($_POST['hour']) || !isset($_POST['motivation'])) error("invalid");
 			if($_SESSION['adult'] || check_role('parent')){
 				if(check_role('parent')){
-					/* //prende la matricola dello studente minorenne
+					//prende la matricola dello studente minorenne
 					$sonParentNumb = $SESSION['son'];
 
-					//prende la matricola dello studente proprietario dell'evento
-					$query = "SELECT U_Matricola FROM evento WHERE Id = ?";
+					//prende la matricola dello studente proprietario dell'evento se la matricola è la stessa della precedente
+					$query = "SELECT U_Matricola FROM evento WHERE Id = ? AND U_Matricola = ?";
 					$result = $db->prepare($query);
-    				$result->execute($id);
-    
-					$row = $result->fetch();
-					$eventStudentNumb = $row['U_Matricola'];
+    				$result->execute([$id, $sonParentNumb]);
 
-					//verifica se la matricola dello studente minorenne e quella dello studente proprietario dell'evento combacino
-					if($sonParentNumb==$eventStudentNumb){ */
+					if($result->rowCount() == 0){
+						error("not_parent");
+					}else{
 						$query = "UPDATE evento SET Motivazione = ?, Stato = 'In attesa' WHERE (Id = ?)";
 						$array = [$motivation, $id];
-					//}else{
-					//	error("not_parent");
-					//}
+					}
 				}else
 					if($_SESSION['adult']){
-						/* //prende la matricola dello studente proprietario dell'evento
-						$query = "SELECT U_Matricola FROM evento WHERE Id = ?";
+						//prende la matricola dello studente proprietario dell'evento
+						$query = "SELECT U_Matricola FROM evento WHERE Id = ? AND U_Matricola = ?";
 						$result = $db->prepare($query);
-						$result->execute($id);
-		
-						$row = $result->fetch();
-						$eventStudentNumb = $row['U_Matricola'];
+						$result->execute([$id, $_SESSION['user']]);
 
 						//verifica se la matricola dello studente in sessione e quella dello studente proprietario dell'evento combacino
-						if($_SESSION['user']==$eventStudentNumb){ */
+						if($result->rowCount() == 0){
+							error("not_owner");
+						}else{
 							$query = "UPDATE evento SET Motivazione = ?, Stato = 'In attesa' WHERE (Id = ?)";
 							$array = [$motivation, $id];
-						//}else{
-						//	error("not_owner");
-						//}
+						}
 					}
 			}else{
 				error("insufficient_permission");
@@ -62,14 +55,6 @@
 		
 		case 'r': //remove
 			if(check_role('admin')){
-				/* //prende la classe di appartenenza dell'admin
-				$query = "SELECT U_Matricola, C_Id FROM appartenere INNER JOIN classi ON (appartenere.C_Id = classe.Id) WHERE U_Matricola = ?";
-				$result = $db->prepare($query);
-				$result->execute($_SESSION['user']);
-
-				$row = $result->fetch();
-				$adminClass = $row['C_Id'];
-
 				//prende la matricola dello studente proprietario dell'evento
 				$query = "SELECT U_Matricola FROM evento WHERE (Id = ?)";
 				$result = $db->prepare($query);
@@ -85,14 +70,19 @@
 
 				$row = $result->fetch();
 				$studentClass = $row['C_Id'];
-				
+
+				//prende la classe di appartenenza dell'admin
+				$query = "SELECT U_Matricola, C_Id FROM appartenere INNER JOIN classi ON (appartenere.C_Id = classe.Id) WHERE U_Matricola = ? AND C_Id = ?";
+				$result = $db->prepare($query);
+				$result->execute([$_SESSION['user'], $studentClass]);
+			
 				//verifica che la classe dell'admin in sessione combaci con quella dello studente proprietario dell'evento
-				if($adminClass == $studentClass){ */
+				if($result->rowCount() == 0){
+					error("not_same_class");
+				}else{
 					$query = "DELETE FROM evento WHERE (Id = ?)";
 					$array = [$id];
-				//}else{
-				//	error("not_same_class");
-				//}
+				}
 			}else{
 				error("insufficient_permission");
 			}
@@ -101,18 +91,10 @@
 		case 'a': //approve
 			if(!isset($_POST['date']) || !isset($_POST['hour']) || !isset($_POST['motivation'])) error("invalid");
 			if(check_role('admin')){
-				/* //prende la classe di appartenenza dell'admin
-				$query = "SELECT U_Matricola, C_Id FROM appartenere INNER JOIN classi ON (appartenere.C_Id = classe.Id) WHERE U_Matricola = ?";
-				$result = $db->prepare($query);
-				$result->execute($_SESSION['user']);
-
-				$row = $result->fetch();
-				$adminClass = $row['C_Id'];
-
 				//prende la matricola dello studente proprietario dell'evento
 				$query = "SELECT U_Matricola FROM evento WHERE (Id = ?)";
 				$result = $db->prepare($query);
-				$result->execute($id);
+				$result->execute([$id]);
 
 				$row = $result->fetch();
 				$studentNumb = $row['U_Matricola'];
@@ -120,17 +102,22 @@
 				//prende la classe dello studente
 				$query = "SELECT U_Matricola, C_Id, FROM appartenere INNER JOIN classi ON (appartenere.C_Id = classe.Id) WHERE U_Matricola = ?";
 				$result = $db->prepare($query);
-				$result->execute($_SESSION['user']);
+				$result->execute([$_SESSION['user']]);
 
 				$row = $result->fetch();
 				$studentClass = $row['C_Id'];
+				
+				//prende la classe di appartenenza dell'admin
+				$query = "SELECT U_Matricola, C_Id FROM appartenere INNER JOIN classi ON (appartenere.C_Id = classe.Id) WHERE U_Matricola = ? AND C_Id = ?";
+				$result = $db->prepare($query);
+				$result->execute([$_SESSION['user'], $studentClass]);
 
-				if($adminClass == $studentClass){ */
-					$query = "UPDATE evento SET  Stato = 'Accettato' WHERE (Id = ?)";
+				if($result->rowCount() == 0){
+					error("not_same_class");
+				}else{
+					$query = "UPDATE evento SET Stato = 'Accettato' WHERE (Id = ?)";
 					$array = [$id];
-				//}else{
-				//	error("not_same_class");
-				//}
+				}
 			}else{
 				error("insufficient_permission");
 			}
@@ -139,18 +126,10 @@
 		case 'd': //deny
 			if(!isset($_POST['date']) || !isset($_POST['hour']) || !isset($_POST['motivation'])) error("invalid");
 			if(check_role('admin')){
-				/* //prende la classe di appartenenza dell'admin
-				$query = "SELECT U_Matricola, C_Id FROM appartenere INNER JOIN classi ON (appartenere.C_Id = classe.Id) WHERE U_Matricola = ?";
-				$result = $db->prepare($query);
-				$result->execute($_SESSION['user']);
-
-				$row = $result->fetch();
-				$adminClass = $row['C_Id'];
-
 				//prende la matricola dello studente proprietario dell'evento
 				$query = "SELECT U_Matricola FROM evento WHERE (Id = ?)";
 				$result = $db->prepare($query);
-				$result->execute($id);
+				$result->execute([$id]);
 
 				$row = $result->fetch();
 				$studentNumb = $row['U_Matricola'];
@@ -158,17 +137,22 @@
 				//prende la classe dello studente
 				$query = "SELECT U_Matricola, C_Id, FROM appartenere INNER JOIN classi ON (appartenere.C_Id = classe.Id) WHERE U_Matricola = ?";
 				$result = $db->prepare($query);
-				$result->execute($_SESSION['user']);
+				$result->execute([$_SESSION['user']]);
 
 				$row = $result->fetch();
 				$studentClass = $row['C_Id'];
 
-				if($adminClass == $studentClass){ */
+				//prende la classe di appartenenza dell'admin
+				$query = "SELECT U_Matricola, C_Id FROM appartenere INNER JOIN classi ON (appartenere.C_Id = classe.Id) WHERE U_Matricola = ? AND C_Id = ?";
+				$result = $db->prepare($query);
+				$result->execute([$_SESSION['user'], $studentClass]);
+				
+				if($result->rowCount() == 0){
+					error("not_same_class");
+				}else{
 					$query = "UPDATE evento SET Stato = 'Rifiutato' WHERE (Id = ?)";
 					$array = [$id];
-				//}else{
-				//	error("not_same_class");
-				//}
+				}
 			}else{
 				error("insufficient_permission");
 			}
@@ -182,4 +166,4 @@
 		error("PDO_Query_Exception");
 	}
 	 
-	?>
+?>
