51dcd6ce217e8d1106e9980f6438f2b3af2e515a
DarkWalker19 <rickyserci@gmail.com>
2022-05-24 21:47:12 +0200

    Update addUscita.php

--- addUscita.php
+++ addUscita.php
@@ -5,7 +5,7 @@
 
 	if(check_role("parent")||$_SESSION['adult']){
 	
-		if(!isset(!isset($_POST['hour']) || !isset($_POST['motivation'])) error("invalid");
+		if(!isset($_POST['hour']) || !isset($_POST['motivation'])) error("invalid");
 
 		$db = get_PDO_connection();
 		$hour = $_POST['hour'];
@@ -13,11 +13,11 @@
 		$numb = $_POST['matricola'];
 		
 		$query = "INSERT INTO evento (Stato, Data, Ora_uscita, Motivazione, Tipo, U_Matricola)";
-		$query .= "VALUES (4, CURRENT_DATE, ?, ?, 3, ?);";
+		$query .= "VALUES (4, ?, ?, ?, 3, ?);";
 
 		try{
 			$result = $db->prepare($query);
-			$result->execute([$hour, $motivation, $numb]);
+			$result->execute([$date, $hour, $motivation, $numb]);
 		}catch(PDOException $e){
 			error("PDO_QUERY_Exception");
 		}
