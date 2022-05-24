1cd40557b11a3870acec24795af572bbe4e93e89
DanieleAgati <102727095+DanieleAgati@users.noreply.github.com>
2022-05-24 22:32:10 +0200

    Updates

--- addAssenza.php
+++ addAssenza.php
@@ -9,13 +9,21 @@
 	$db = get_PDO_connection();
 	$numb = $_POST['matricola'];
 
-	$query = "INSERT INTO evento (Stato, Data, Tipo, U_Matricola)";
-	$query .= "VALUES (4, CURRENT_DATE, 1, ?)";
-	
-	try{
-		$result = $db->prepare($query);
-		$result->execute([$numb]);
-	}catch(PDOException $e){
-		error("PDO_QUERY_Exception");
+	//verifica se è presente un evento su un determinato studente
+	$query = "SELECT U_Matricola FROM evento WHERE Data != CURRENT_DATE AND Tipo != 'Uscita Anticipata'";
+	$result = $db->prepare($query);
+    $result->execute();
+
+	if($result->rowCount() == 0){
+		error("not_same_class");
+	}else{
+		$query = "INSERT INTO evento (Stato, Data, Tipo, U_Matricola)";
+		$query .= "VALUES (4, CURRENT_DATE, 1, ?)";
+		try{
+			$result = $db->prepare($query);
+			$result->execute([$numb]);
+		}catch(PDOException $e){
+			error("PDO_QUERY_Exception");
+		}
 	}
 ?>
