f88cf3c2c0daa54611a542fd27250845107b4c6e
DarkWalker19 <rickyserci@gmail.com>
2022-05-24 22:42:25 +0200

    Update addAssenza.php

--- addAssenza.php
+++ addAssenza.php
@@ -10,12 +10,11 @@
 	$numb = $_POST['matricola'];
 
 	//verifica se è presente un evento su un determinato studente
-	$query = "SELECT U_Matricola FROM evento WHERE Data != CURRENT_DATE AND Tipo != 'Uscita Anticipata'";
-	$result = $db->prepare($query);
-    $result->execute();
+	$query = "SELECT U_Matricola FROM evento WHERE Data = CURRENT_DATE AND Tipo != 'Uscita'";
+	$result = $db->query($query);
 
-	if($result->rowCount() == 0){
-		error("not_same_class");
+	if($result->rowCount() < 0){
+		error("event_already_exists");
 	}else{
 		$query = "INSERT INTO evento (Stato, Data, Tipo, U_Matricola)";
 		$query .= "VALUES (4, CURRENT_DATE, 1, ?)";
