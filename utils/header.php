9207da199520e5609aabe6e919468d015f2c1ed7
DanieleAgati <102727095+DanieleAgati@users.noreply.github.com>
2022-05-25 23:51:15 +0200

    class

--- utils/header.php
+++ utils/header.php
@@ -25,7 +25,7 @@
             if(!isset($_SESSION['user'])){
                 echo    '<nav class="navbarBackground navbar navbar-expand-lg">
                             <div class="container-fluid divLoginButton">';
-                echo            "<button class=\"customButton btn btn-primary\" onclick=\"location.href='login.php'\" type=\"button\">Login</button>
+                echo            "<button class=\"btn btn-primary customButton\" onclick=\"location.href='login.php'\" type=\"button\">Login</button>
                             </div>
                         </nav>";
             }else
@@ -35,30 +35,30 @@
                                     
                                         <ul class="navbar-nav">
                                             <li class="nav-item">
-                                                <a class="nav-link" style="color: white;" href = \'agenda.php?section=a\'>Assenze</a>
+                                                <a class="nav-link divLink" href = \'agenda.php?section=a\'>Assenze</a>
                                             </li>
                                             <li class="nav-item">
-                                                <a class="nav-link" style="color: white;" href = \'agenda.php?section=r\'>Ritardi</a>
+                                                <a class="nav-link divLink" href = \'agenda.php?section=r\'>Ritardi</a>
                                             </li>           
                                             <li class="nav-item">
-                                                <a class="nav-link" style="color: white;" href = \'agenda.php?section=u\'>Uscite anticipate</a>
+                                                <a class="nav-link divLink" href = \'agenda.php?section=u\'>Uscite anticipate</a>
                                             </li>
                                         </ul>
                                     
                                 </div>';        
-                    echo            "<button class=\"btn btn-primary\" style=\"background-color: #98CCFF;\" onclick=\"location.href='logout.php'\" type=\"button\" align=\"right\" hspace=\"10\">Logout</button>
+                    echo            "<button class=\"btn btn-primary customButton\" onclick=\"location.href='logout.php'\" type=\"button\" align=\"right\" hspace=\"10\">Logout</button>
                             </nav>";
                 }else{
-                    echo    '<nav class="navbar navbar-expand-lg" style="background-color: #6699FF; display: flex; justify-content: flex-end;">
+                    echo    '<nav class="navbar navbar-expand-lg " style="background-color: #6699FF; display: flex; justify-content: flex-end;">
                                 <div class="container-fluid">
                                    
                                         <ul class="navbar-nav">
                                             <li class="nav-item">
-                                                <a class="nav-link" style="color: white;" href = \'agenda.php?section=c\'>Le mie classi</a>
+                                                <a class="nav-link divLink" href = \'agenda.php?section=c\'>Le mie classi</a>
                                             </li>
                                     
                                 </div>';
-                    echo            "<button class=\"btn btn-primary\" style=\"background-color: #98CCFF;\" onclick=\"location.href='logout.php'\" type=\"button\" align=\"right\" hspace=\"10\">Logout</button>
+                    echo            "<button class=\"btn btn-primary customButton\" onclick=\"location.href='logout.php'\" type=\"button\" align=\"right\" hspace=\"10\">Logout</button>
                             </nav>";
                     }
 
