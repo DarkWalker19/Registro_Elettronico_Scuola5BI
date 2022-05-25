6f63f27221c575bae2ec53fdadc499f432087b42
DarkWalker19 <rickyserci@gmail.com>
2022-05-25 23:13:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
         <?php
             print_header();
             if((!isset($_SESSION['name'])) || ($_SESSION['name'] == null)){
-                echo "<div class='_abs-center'>
+                echo "<div class='_center'>
                         <h4>Non sei loggato! Clicca <b>Login</b> in alto per accedere.</h4>
                     </div>";
             }else{
-                echo "<div class='_abs-center'>
+                echo "<div class='_center'>
                         <h4>Benvenuto". " " . "<i>" . $_SESSION['name'] . "</i></h4>
                     </div>";
             }
